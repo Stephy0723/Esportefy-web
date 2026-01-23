6336a16aab897e2a8645765fbcaaf105def5f515
Arlington Feliz <felizarlington@gmail.com>
2026-01-23 02:59:38 -0400

    TASK: Implementation of a logger to track backend requests

--- Backend/server.js
+++ Backend/server.js
@@ -5,15 +5,4 @@ const PORT = process.env.PORT || 4000;
 
 app.listen (PORT, ()=>{
     console.log(`Servidor corriendo endpoint http://localhost:${PORT}`);
-    console.log('Rutas disponibles:');
-    console.log(`http://localhost:${PORT}/api/auth/register`);
-    console.log(`http://localhost:${PORT}/api/auth/login`);
-    console.log(`http://localhost:${PORT}/api/auth/profile`);
-    console.log(`http://localhost:${PORT}/api/auth/update-profile`);
-    console.log(`http://localhost:${PORT}/api/auth/forgot-password`);
-    console.log(`http://localhost:${PORT}/api/auth/reset-password/:token`);
-    console.log(`http://localhost:${PORT}/api/teams/create`);
-    console.log(`http://localhost:${PORT}/api/teams/join`);
-    console.log(`http://localhost:${PORT}/api/tournaments/`);
-    console.log(`http://localhost:${PORT}/api/tournaments/create`);
 });
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
+import { logger } from './middlewares/logger.js';
 
 import authRoutes from './routes/auth.routes.js';
 import teamRoutes from './routes/team.routes.js';
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', authRoutes);
